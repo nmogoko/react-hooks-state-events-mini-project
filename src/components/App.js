7a2fb68b0aa99b673faecb388b332ebd8f5785c8
nmogoko <nancymogoko@gmail.com>
2024-08-21 17:24:07 +0100

    Completed labs

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,44 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // Function to handle category selection
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  // Handle form submission to add a new task
+  const onTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  // Filter tasks based on the selected category
+  const filteredTasks =
+    selectedCategory === "All"
+      ? tasks
+      : tasks.filter((task) => task.category === selectedCategory);
+
+  // Function to handle deleting a task
+  const handleDelete = (index) => {
+    const remainingTasks = tasks.filter((task, i) => i !== index);
+    setTasks(remainingTasks);
+  };
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onCategoryClick={handleCategoryClick}
+      />
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={onTaskFormSubmit}
+      />
+      <TaskList tasks={filteredTasks} onDelete={handleDelete} />
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { TASKS } from "../data";
 
-function Task() {
+function Task(props) {
+  const handleDelete = () => {
+    props.onDelete(props.index);
+  };
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{props.category}</div>
+      <div className="text">{props.text}</div>
+      <button className="delete" onClick={handleDelete}>
+        X
+      </button>
     </div>
   );
 }
